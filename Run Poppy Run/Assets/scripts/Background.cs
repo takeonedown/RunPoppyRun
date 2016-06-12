94d086719debc6f4446f83e8af5266ec1acf75be
Chris Leung <takeonedown115@yahoo.ca>
2016-06-11 22:40:33 -0600

    Moving Background

--- Run Poppy Run/Assets/scripts/Background.cs
+++ Run Poppy Run/Assets/scripts/Background.cs
@@ -1,20 +1,20 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Background : MonoBehaviour {
+public class background : MonoBehaviour {
 
 	public float speed;
-
+	public Renderer rend;
+	
 	// Use this for initialization
 	void Start () {
-	
+		rend = GetComponent<Renderer>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		Vector2 offset = new Vector2 (Time.time * speed)
-		
-		GetComponent<Renderer>().material.mainTextureOffset = offset;﻿
+		float offset = Time.time * speed;
+		rend.material.mainTextureOffset = new Vector2(offset, 0);
 	
 	}
 }
---
+++ Run Poppy Run/Assets/scripts/PlayerController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+	public float jump = 10;
+	bool nojump = false;
+
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0) ) {
+			GetComponent<Rigidbody2D>().velocity = new Vector2 
+			(GetComponent<Rigidbody2D>().velocity.x, jump);
+		}
+	}
+	
+	void onTriggerEnter2D() {
+		nojump = true;
+	}
+	void onTriggerExit2D() {
+		nojump = false;
+	}
+	
+}
